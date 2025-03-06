b05e7708ea1f33b0d1a41658ea8ca8dab61b3b43
ndayingiye <ndayiringiyedavid120@gmail.com>
2025-03-06 10:30:23 +0200

    feat/count vowels

--- array.js
+++ array.js
@@ -1,5 +1,17 @@
-function reverseString(str){
+function reverseString(str) {
     const newArray = str.split("").reverse().join("")
     return newArray;
 }
 console.log(reverseString("happy coding and great job"));
+
+function countVowels(strin) {
+    let count = 0;
+    const vowels = "aeiou"
+    for (let char of strin) {
+        if (vowels.includes(char)) {
+            count++
+        }
+    }
+    return count;
+}
+console.log(countVowels("a e i o u"));
